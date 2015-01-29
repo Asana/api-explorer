0a3c01329bd83a38c973e6ea364385ae5ca23fc8
Eric Pelz <eric@asana.com>
2015-01-29 14:17:35 -0800

    Collapse module declarations into "asana" module
    - Collapse all module declarations from the declared `Asana` module into `"asana"`
    - Fix `Asana.Client.create()` to be static
    - Make `Asana.auth`, `Asana.errors`, and `Asana.resources` be modules.
    
    As a test that this works, I added type definitions for `Asana.auth.PopupFlow`.
    
    Thanks @pspeter3 and @vsiao for comments and help!

--- src/asana.d.ts
+++ src/asana.d.ts
@@ -2,57 +2,26 @@
 // Project: https://github.com/asana/node-asana/
 // Note: These type definitions are incomplete; only used sections of the API will be defined.
 
-declare module Asana {
-
-    // This is an external library, so we can't enforce PascalCased class and interface names.
-    /* tslint:disable:class-name */
-
-    //
-    // Asana.Client API
-    // ----------------------------------------------------------------------
-    class Client {
-        constructor(dispatcher: Dispatcher, options?: any);
-        create(options?: any): Client;
-    }
-
-    //
-    // Asana.Dispatcher API
-    // ----------------------------------------------------------------------
-    class Dispatcher {
-    }
-
-    //
-    // Asana.auth API
-    // ----------------------------------------------------------------------
-    interface auth {
-    }
-
-    //
-    // Asana.errors API
-    // ----------------------------------------------------------------------
-    interface errors {
-    }
-
-    //
-    // Asana.resources API
-    // ----------------------------------------------------------------------
-    interface resources {
-    }
-
-    //
-    // Asana Exports
-    // ----------------------------------------------------------------------
-    interface Exports {
-        Client: Client;
-        Dispatcher: Dispatcher;
-        auth: auth;
-        errors: errors;
-        resources: resources;
-    }
-}
-
 declare module "asana" {
-    var exports: Asana.Exports;
-    export = exports;
-}
 
+    export class Client {
+        constructor(dispatcher: Dispatcher, options?: any);
+        static create(options?: any): Client;
+    }
+
+    export class Dispatcher {
+    }
+
+    export module auth {
+        export class PopupFlow {
+            constructor(options?: any);
+            static runReceiver(): void;
+        }
+    }
+
+    export module errors {
+    }
+
+    export module resources {
+    }
+}
