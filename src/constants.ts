1b774ea555ce504933a36a830c6f88253e49677e
Eric Pelz <eric@asana.com>
2015-01-30 01:10:06 -0800

    Clean up oauth flow
    - Isolate client authorization to getter function. This is cleaner
      and will let us do better tests
    - Move constants to a separate file
    - Convert js functions to typescript
    - Adds documentation to functions

---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+export var CLIENT_ID: string = "23824292948206";
+export var REDIRECT_URI: string = "http://localhost:8338/popup_receiver.html";
--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,22 @@
 import Asana = require("asana");
 /// <reference path="jquery/jquery.d.ts" />
 import $ = require("jquery");
+import constants = require("./constants");
 
 // TODO: Add tests for this.
 
 /**
- * TODO: WRITE DOCS
- * TODO: Convert to typescript
- * TODO: Isolate into multiple functions
+ * Gets a client and ensures it is authorized to make requests.
+ * Tries to use given credentials; otherwise, uses the popup oauth flow.
+ *
+ * @param credentials  Credentials to pass to the client before authorization
+ * @returns {Promise<Client>}  A promise that resolves to this client when
+ *     authorization is complete.
  */
-export function authorizeAndGetName(credentials?: any): void {
-    var CLIENT_ID = "23824292948206";
-    var REDIRECT_URI = "http://localhost:8338/popup_receiver.html";
-
-    var client = Asana.Client.create({
-        clientId: CLIENT_ID,
-        redirectUri: REDIRECT_URI
+export function authorizedClient(credentials?: any): Promise<Asana.Client> {
+    var client: Asana.Client = Asana.Client.create({
+        clientId: constants.CLIENT_ID,
+        redirectUri: constants.REDIRECT_URI
     });
 
     // Try to use credentials, if they are supplied. Otherwise, we use the popup flow.
@@ -26,9 +27,20 @@ export function authorizeAndGetName(credentials?: any): void {
         flowType: Asana.auth.PopupFlow
     });
 
+    return client.authorize();
+}
+
+/**
+ * Gets the user's name, and sets text in #ui to reflect it.
+ * This is mainly an example, to show how the API and JQuery will interact.
+ * TODO: Remove this after implementing other API tester functionality.
+ *
+ * @param credentials  Credentials to pass to the client before authorization
+ */
+export function getName(credentials?: any): void {
     // Try to authorize, and display the user's name.
     $("#ui").html("Authorizing...");
-    client.authorize().then(function() {
+    authorizedClient().then(function(client) {
         // The client is authorized! Make a simple request.
         $("#ui").html("Fetching...");
         return client.users.me().then(function(me) {
@@ -40,7 +52,8 @@ export function authorizeAndGetName(credentials?: any): void {
 }
 
 /**
- * TODO: WRITE DOCS
+ * Runs the receiver code to send the Oauth result to the requesting tab.
+ * Note: This logic is handled entirely within the `Asana.auth` module.
  */
 export function runReceiver(): void {
     Asana.auth.PopupFlow.runReceiver();
