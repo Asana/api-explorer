0a5837570f0928ebfc5522d34f9fdfe1b066e71a
Eric Pelz <eric@asana.com>
2015-03-12 14:44:12 -0700

    Use object instead of class for constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,22 +1,22 @@
-class BaseConstants {
-  public static LOCALSTORAGE_KEY: string;
-  public static CLIENT_ID: string;
-  public static REDIRECT_URI: string;
+interface BaseConstants {
+  LOCALSTORAGE_KEY: string;
+  CLIENT_ID: string;
+  REDIRECT_URI: string;
 }
 
-class GhPagesConstants implements BaseConstants {
-  public static LOCALSTORAGE_KEY = "api_tester_credentials";
-  public static CLIENT_ID = "29147353239426";
-  public static REDIRECT_URI =
-    "https://asana.github.io/node-asana-tester/popup_receiver.html";
-}
+var ghPagesConstants: BaseConstants = {
+  LOCALSTORAGE_KEY: "api_tester_credentials",
+  CLIENT_ID: "29147353239426",
+  REDIRECT_URI:
+    "https://asana.github.io/node-asana-tester/popup_receiver.html"
+};
 
-class LocalhostConstants implements BaseConstants {
-  public static LOCALSTORAGE_KEY = "api_tester_credentials";
-  public static CLIENT_ID = "23824292948206";
-  public static REDIRECT_URI = "http://localhost:8338/popup_receiver.html";
-}
+var localhostConstants: BaseConstants = {
+  LOCALSTORAGE_KEY: "api_tester_credentials",
+  CLIENT_ID: "23824292948206",
+  REDIRECT_URI: "http://localhost:8338/popup_receiver.html"
+};
 
 var constants = process.env.USE_GH_PAGES ?
-  GhPagesConstants : LocalhostConstants;
+  ghPagesConstants : localhostConstants;
 export = constants;
