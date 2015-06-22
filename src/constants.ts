829433faf5db7231417152a0baae357adb32c212
Andrew Noonan <andrew@noonan.cc>
2015-06-22 16:00:39 -0700

    adding production constants

--- src/constants.ts
+++ src/constants.ts
@@ -19,10 +19,20 @@ var localhostConstants: BaseConstants = {
   INITIAL_PAGINATION_LIMIT: 10
 };
 
+var production: BaseConstants = {
+  LOCALSTORAGE_KEY: "api_explorer_credentials",
+  CLIENT_ID: "38682966449842",
+  REDIRECT_URI: "https://asana.com/popup_receiver.php",
+  INITIAL_PAGINATION_LIMIT: 10
+};
+
 var constant: BaseConstants;
 switch (process.env.CONSTANTS_TYPE) {
   case "gh_pages":
     constant = ghPagesConstants;
+    break;
+  case "production":
+    constant = production;
     break;
   // case "localhost":
   default:
